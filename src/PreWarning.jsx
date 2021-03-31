daa9cebbaeeeea7eee676fd7e8a2ffc181bafeed
andrew-1135 <46470763+andrew-1135@users.noreply.github.com>
2021-03-30 21:42:50 -0700

    Reduced size of NewFlowDialog
    Also added no break spaces in various places

--- src/PreWarning.jsx
+++ src/PreWarning.jsx
@@ -6,22 +6,22 @@ export default function PreWarning({ accept }) {
     <div className="PreWarning">
       <section>
         <h3>⚠️ Important information ⚠️</h3>
-        <p>Prereq Flow is not an official University of Washington resource and is undergoing active development. Any saved data may be become unusable at any time with no warning. No guarantees are made about the accuracy, completeness, or up-to-dateness of any information presented.</p>
+        <p>Prereq Flow is not an official University of Washington resource and is undergoing active development. Any saved data may be become unusable at any time with no warning. No guarantees are made about the accuracy, completeness, or up-to-dateness of any information&nbsp;presented.</p>
 
-        <p>Some limitations to keep in mind:</p>
+        <p>Some limitations to keep in&nbsp;mind:</p>
         <ul>
-          <li>Grouping and either/or conditions are not displayed.</li>
-          <li>Co-requisite condition is not displayed.</li>
-          <li>Registration restrictions are not displayed.</li>
+          <li>Grouping and either/or conditions are not&nbsp;displayed.</li>
+          <li>Co-requisite condition is not&nbsp;displayed.</li>
+          <li>Registration restrictions are not&nbsp;displayed.</li>
         </ul>
 
-        <p>All caveats for <a href="https://prereqmap.uw.edu/" target="_blank" rel="noreferrer">Prereq Map</a> also apply here:</p>
+        <p>All caveats for <a href="https://prereqmap.uw.edu/" target="_blank" rel="noreferrer">Prereq Map</a> also apply&nbsp;here:</p>
         <ul>
-          <li>Prerequisites and graduation requirements may change over time.</li>
-          <li>Non-course graduation requirements (e.g. 5 credits of VLPA) are not displayed.</li>
-          <li>Equivalencies (e.g. placements tests, AP credits) are not displayed.</li>
+          <li>Prerequisites and graduation requirements may change over&nbsp;time.</li>
+          <li>Non-course graduation requirements (e.g. 5 credits of VLPA) are not&nbsp;displayed.</li>
+          <li>Equivalencies (e.g. placements tests, AP credits) are not&nbsp;displayed.</li>
         </ul>
-        <p>Talk to your advisor when course planning.</p>
+        <p>Talk to your advisor when course&nbsp;planning.</p>
         <button type="button" onClick={accept}>Continue</button>
       </section>
     </div>
