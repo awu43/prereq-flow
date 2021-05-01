d47a73a8cd220df676d65cf5072d1bc8f68902da
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-04-30 18:22:58 -0700

    Added busy status to OpenFileDialog

--- src/dialogs/OpenFileDialog.jsx
+++ src/dialogs/OpenFileDialog.jsx
@@ -10,17 +10,22 @@ import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
 
 export default function OpenFileDialog({ modalCls, closeDialog, openFlow }) {
+  const [busy, setBusy] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
   const fileInputRef = useRef(null);
 
   function close() {
+    setBusy(false);
     setErrorMsg("");
     closeDialog();
   }
 
   function validateFile(file) {
+    setBusy(true);
+
     if (file.type !== "application/json") {
       setErrorMsg("Invalid file type");
+      setBusy(false);
       return;
     }
     const reader = new FileReader();
@@ -29,22 +34,30 @@ export default function OpenFileDialog({ modalCls, closeDialog, openFlow }) {
     let loadedData;
     reader.onload = event => {
       loadedData = JSON.parse(event.target.result);
+
       const structureValid = [
         typeof loadedData === "object",
         Object.keys(loadedData).toString() === ["version", "elements"].toString()
       ].every(a => a);
+
       if (!structureValid) {
         setErrorMsg("Invalid data");
+        setBusy(false);
         return;
       }
+
       const { version, elements } = loadedData;
       const dataValid = [
         typeof version === "string",
         Array.isArray(elements),
         elements.every(e => isNode(e) || isEdge(e)),
       ].every(a => a);
+
       if (!dataValid) {
         setErrorMsg("Invalid data");
+        setBusy(false);
+        // eslint-disable-next-line no-useless-return
+        return;
       } else {
         openFlow(elements);
         close();
@@ -70,8 +83,12 @@ export default function OpenFileDialog({ modalCls, closeDialog, openFlow }) {
       }}
     >
       <DialogContent className="OpenFileDialog" aria-label="Open file dialog">
-        {/* TODO: Add busy status */}
-        <button type="button" className="close-button" onClick={close}>
+        <button
+          type="button"
+          className="close-button"
+          onClick={close}
+          disabled={busy}
+        >
           <img src="dist/icons/x-black.svg" alt="close" />
         </button>
         <section>
@@ -86,12 +103,18 @@ export default function OpenFileDialog({ modalCls, closeDialog, openFlow }) {
             offset={[0, 5]}
             visible={errorMsg.length}
           >
-            <input type="file" accept="application/json" ref={fileInputRef} />
+            <input
+              ref={fileInputRef}
+              type="file"
+              accept="application/json"
+              disabled={busy}
+            />
           </Tippy>
           <button
             className="OpenFileDialog__open-button"
             type="button"
             onClick={openFile}
+            disabled={busy}
           >
             Open
           </button>
--- src/index.jsx
+++ src/index.jsx
@@ -16,6 +16,7 @@ if (import.meta.hot) {
   import.meta.hot.accept();
 }
 
+// TODO: Stop dialogs from exiting when busy
 // TODO: Degree select dialog (select courses)
 // TODO: Course add dialog (add connections)
 // TODO: Mobile/tablet warning
