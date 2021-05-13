5031ef55538e5241b0a8f889d1013b1ce8d56dee
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-05-12 23:13:42 -0700

    Created OrNode component
    Hasn't been used yet, only created

---
+++ src/OrNode.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Handle } from "react-flow-renderer";
+
+const orNodeStyle = {
+  padding: "10px",
+  // "border-radius": "3px",
+  // minWidth: "150px",
+  // "fontSize": "12px",
+  // color: "#222",
+  textAlign: "center",
+  // "border-width": "1px",
+  borderStyle: "solid",
+};
+// TEST
+export default function OrNode({ data }) {
+  return (
+    <div
+      className={`OrNode ${data.nodeStatus}${
+        data.nodeConnected ? " connected" : ""}`}
+      style={orNodeStyle}
+    >
+      <Handle type="target" position="left" />
+      <div>OR</div>
+      <Handle type="source" position="right" />
+    </div>
+  );
+}
+OrNode.propTypes = {
+  data: PropTypes.shape({
+    nodeStatus: PropTypes.string,
+    nodeConnected: PropTypes.bool,
+  }).isRequired,
+};
