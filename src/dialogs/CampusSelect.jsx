4ba67b705bb3ac3fb9760bde607ae902c41e3d21
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-05-26 18:28:10 -0700

    Split CampusSelect styling into separate file

--- src/dialogs/CampusSelect.jsx
+++ src/dialogs/CampusSelect.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import "./CampusSelect.scss";
+
 export default function CampusSelect({
   selectedCampus, setSelectedCampus, busy
 }) {
