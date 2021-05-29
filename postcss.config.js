f695dd351dd33ad7c1ecd1ec43bb7f05b52d9ea4
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-05-28 20:01:14 -0700

    Changed PostCSS config to use autoprefixer in dev
    Still stops the warnings, but faster than full preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   plugins: [
-    require('postcss-preset-env')
     // https://flaviocopes.com/tailwind-setup/
     // In development, avoid too much processing
-    // process.env.NODE_ENV === 'production'
-    //   ? require('postcss-preset-env')
-    //   : null,
+    process.env.NODE_ENV === 'production'
+      ? require('postcss-preset-env')
+      : require('autoprefixer'),
     // process.env.NODE_ENV === 'production' ? require('cssnano') : null,
   ],
 };
