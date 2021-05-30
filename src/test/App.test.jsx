73e7eb817fe95110ac7aa09d19115e78945f4c77
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-05-30 13:54:08 -0700

    Converted index to TS

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import { render } from "@testing-library/react";
-import { expect } from "chai";
-import App from "../App";
+// import * as React from "react";
+// import { render } from "@testing-library/react";
+// import { expect } from "chai";
+// import App from "../App";
 
 // describe("<App>", () => {
 //   it("renders learn react link", () => {
