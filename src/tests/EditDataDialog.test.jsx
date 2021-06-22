270f4f5e4e90778d4e4cccfa95b5199f2bf5c3a3
andrew-1135 <46470763+awu43@users.noreply.github.com>
2021-06-21 21:40:06 -0700

    Added edge testing to EditDataDialog tests

--- src/tests/EditDataDialog.test.jsx
+++ src/tests/EditDataDialog.test.jsx
@@ -14,6 +14,7 @@ import {
   getNode,
   clickContextOption,
 } from "./react-test-utils";
+import { TEST_COND_IDS } from "./test-utils";
 
 describe("<EditDataDialog />", () => {
   it("Disables save button when ID field is empty", () => {
@@ -40,7 +41,7 @@ describe("<EditDataDialog />", () => {
     expect(saveButton.getAttribute("disabled")).to.not.be.null;
   });
   it("Saves new course ID", async () => {
-    const { container } = render(newApp());
+    const { container, queryByTestId } = render(newApp());
     const MATH_125 = getNode("MATH 125", container);
     clickContextOption("Edit data", MATH_125, container);
     const dialog = document.querySelector(".EditDataDialog");
@@ -54,5 +55,11 @@ describe("<EditDataDialog />", () => {
     ));
     expect(getNode("MATH 125", container)).to.be.null;
     expect(getNode("FOO 123", container)).to.not.be.null;
+    expect(queryByTestId("MATH 125 -> MATH 307")).to.be.null;
+    expect(queryByTestId(`MATH 125 -> ${TEST_COND_IDS.OR1}`)).to.be.null;
+    expect(queryByTestId("MATH 125 -> MATH 126")).to.be.null;
+    expect(queryByTestId("FOO 123 -> MATH 307")).to.not.be.null;
+    expect(queryByTestId(`FOO 123 -> ${TEST_COND_IDS.OR1}`)).to.not.be.null;
+    expect(queryByTestId("FOO 123 -> MATH 126")).to.not.be.null;
   });
 });
