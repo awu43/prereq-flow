ecd0cbab9076adaa9a48644f1abbc269635b113f
awu43 <46470763+awu43@users.noreply.github.com>
2021-08-14 14:30:12 -0700

    Lint OrNode

--- src/components/OrNode.tsx
+++ src/components/OrNode.tsx
@@ -6,12 +6,12 @@ import { Handle, Position } from "react-flow-renderer";
 
 import type { BaseNodeData } from "types/main";
 
-export default function OrNode({ data }: { data: BaseNodeData }) {
+export default function OrNode({ data }: { data: BaseNodeData }): JSX.Element {
   return (
     <div
-      className={classNames(
-        "OrNode", data.nodeStatus, { connected: data.nodeConnected }
-      )}
+      className={classNames("OrNode", data.nodeStatus, {
+        connected: data.nodeConnected,
+      })}
     >
       <Handle type="target" position={Position.Left} />
       <div>OR</div>
