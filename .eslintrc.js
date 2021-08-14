9974c847221a6564b1ad27f81b6344089348827c
awu43 <46470763+awu43@users.noreply.github.com>
2021-08-14 12:05:57 -0700

    Add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,83 +2,37 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    mocha: true,
   },
   extends: [
     "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "airbnb-typescript",
+    "airbnb",
+    "plugin:prettier/recommended",
   ],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
     sourceType: "module",
-    project: "./tsconfig.json",
   },
-  plugins: [
-    "react",
-    "react-hooks",
-    "@typescript-eslint",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-    "no-restricted-syntax": [
-      "error", "ForInStatement", "LabeledStatement", "WithStatement"
-    ],
-    "comma-dangle": ["error", "only-multiline"],
-    "arrow-parens": ["error", "as-needed"],
-    "no-param-reassign": ["error", { props: false }],
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    // "no-unused-vars": ["error", {
-    //   varsIgnorePattern: "^_",
-    //   argsIgnorePattern: "^_",
-    // }],
-    "prefer-const": ["error", { destructuring: "all" }],
-    "no-underscore-dangle": "off",
-    "no-else-return": "off",
-    "object-curly-newline": "off",
-    "no-continue": "off",
-    "import/no-named-default": "off",
-
-    "react/jsx-one-expression-per-line": "off",
-    "react/destructuring-assignment": "off",
-    "react/jsx-boolean-value": ["error", "always"],
-    "react/no-unescaped-entities": ["error", { forbid: [">"] }],
-    "react/self-closing-comp": ["error", { component: true, html: false }],
-    "jsx-a11y/label-has-associated-control": ["error", { assert: "either" }],
-    "max-len": "off",
-
-    "@typescript-eslint/quotes": ["error", "double"],
-    "@typescript-eslint/comma-dangle": ["error", "only-multiline"],
-    // "no-use-before-define": "off",
-    // "@typescript-eslint/no-use-before-define": ["error"],
-    // "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error", {
-      varsIgnorePattern: "^_",
-      argsIgnorePattern: "^_",
-    }],
-    "@typescript-eslint/naming-convention": "off",
-    "react/props-types": "off",
-    "react/require-default-props": "off",
-  },
+  plugins: ["react"],
+  rules: {},
   overrides: [
     {
-      files: ["**/*.test.{jsx,js}"],
-      rules: {
-        "no-undef": "off",
-        "@typescript-eslint/no-unused-expressions": "off",
-      },
+      files: ["**/*.ts?(x)"],
+      extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+      ],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      rules: {},
     },
     {
       files: ["**/*.spec.js"],
-      plugins: [
-        "cypress",
-      ],
-      extends: [
-        "plugin:cypress/recommended",
-      ],
+      plugins: ["cypress"],
+      extends: ["plugin:cypress/recommended"],
     },
-  ]
+  ],
 };
